12b61b001dbc9116fd411b7b7e7a1e086c909a2c
ethkri <160322768+ethkri@users.noreply.github.com>
2024-02-28 15:23:18 +0530

    Update helper-hardhat-config.ts

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -1,8 +1,8 @@
 export const CONTRACT = "0x84bF528F76AbEa335dC2e5ae0f92abBC30808A8c";
 export const DEPLOYER = "0x0aBCaDe3C4cB5f2F1e6b192A2bf2DB2b4E442205";
 export const RECIEVER = "0xbEeF6335052e672680D3D73dc4129535820bD141";
-export const AMOUNT_TO_MINT = 50;
-export const AMOUNT_TO_TRANSFER = 20;
-export const AMOUNT_TO_BURN = 30;
-export const ITEM_NAME = "Tee";
+export const AMOUNT_TO_MINT = 500;
+export const AMOUNT_TO_TRANSFER = 200;
+export const AMOUNT_TO_BURN = 50;
+export const ITEM_NAME = "Cap";
 export const ITEM_PRICE = 15;
